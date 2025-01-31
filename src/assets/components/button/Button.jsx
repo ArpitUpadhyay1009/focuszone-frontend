2f09ae979fb826a77e55d97cb5b183f68181f5c2
ArpitUpadhyay1009 <arpitupadhyay186@gmail.com>
2025-01-31 11:26:32 +0530

    changed font weight

--- src/assets/components/button/Button.jsx
+++ src/assets/components/button/Button.jsx
@@ -5,7 +5,7 @@ export const Button = () => {
   return (
     <>
       <a href="/">
-        <button className="bg-[#7500CA] text-white font-[Poppins] px-4 py-2 md:px-5 md:py-2 sm:px-4 sm:py-2 text-lg md:text-base sm:text-sm rounded-lg transition-all hover:scale-105 duration-300 cursor-pointer">
+        <button className="bg-[#7500CA] text-white font-medium font-[Poppins] px-4 py-2 md:px-5 md:py-2 sm:px-4 sm:py-2 text-lg md:text-base sm:text-sm rounded-lg transition-all hover:scale-105 duration-300 cursor-pointer">
           Login/Register
         </button>
       </a>
