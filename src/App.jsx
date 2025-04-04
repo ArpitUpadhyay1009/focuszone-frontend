d28d3d33411bc264d69adee0b54e92ec8ad5ec06
ArpitUpadhyay1009 <arpitupadhyay186@gmail.com>
2025-04-04 17:01:28 +0530

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Dashboard from "./assets/pages/dashboard/Dashboard";
 import Login from "./assets/pages/login/Login";
 import Register from "./assets/pages/register/Register";
 import ForgotPassword from "./assets/pages/forgotPassword/ForgotPassword";
-import ResetPassword from "./assets/pages/resetPassword/resetPassword";
+// import ResetPassword from "./assets/pages/resetPassword/resetPassword";
 import VerifyOtp from "./assets/pages/verifyotp/VerifyOtp";
 function App() {
   return (
@@ -16,7 +16,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot" element={<ForgotPassword />} />
-          <Route path="/reset" element={<ResetPassword />} />
           <Route path="/verify-otp" element={<VerifyOtp />} />
           <Route path="/home" element={<Dashboard />} />
         </Routes>
