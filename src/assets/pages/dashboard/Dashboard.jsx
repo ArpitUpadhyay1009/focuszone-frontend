78d7c760773a18e7b02dcc9ed8ebb69ed367d11b
ArpitUpadhyay1009 <arpitupadhyay186@gmail.com>
2025-04-10 21:37:17 +0530

    changed positions of level renders

--- src/assets/pages/dashboard/Dashboard.jsx
+++ src/assets/pages/dashboard/Dashboard.jsx
@@ -13,11 +13,11 @@ export const Home = () => {
       <div>
         <Navbar2 />
       </div>
-      <div className="flex flex-col md:flex-row flex-wrap justify-center items-center space-y-10 md:space-y-0 md:space-x-40">
-        <div className="mt-5 mb-60 ml-2 md:ml-5 lg:mt-1 lg:ml-15">
+      <div className="flex flex-col md:flex-row flex-wrap justify-center items-center space-y-0 md:space-y-0 md:space-x-40">
+        <div className="mt-20 mb-5 ml-2 md:ml-5 lg:mt-1 lg:ml-15">
           <TimerApp />
         </div>
-        <div className="mt-2 mr-5 md:mr-5 lg:mr-20">
+        <div className="mt-0 mr-5 md:mr-5 lg:mr-20">
           <LevelRenders />
           <LevelUpgradeSystem />
           <SpotifyEmbed />
