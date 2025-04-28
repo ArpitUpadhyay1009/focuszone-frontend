36272b3ccfd05283758f66ce6ab712ab95634a25
ArpitUpadhyay1009 <arpitupadhyay186@gmail.com>
2025-04-29 00:01:29 +0530

    fixed spotify homepage

--- src/assets/components/spotify/SpotifyEmbed.jsx
+++ src/assets/components/spotify/SpotifyEmbed.jsx
@@ -1,21 +1,20 @@
 import "./SpotifyEmbed.css";
 import React from "react";
 
-const SpotifyEmbed = () => {
+const SpotifyEmbed2 = () => {
   return (
     <>
       <div>
         <iframe
           src="https://open.spotify.com/embed/playlist/0oPyDVNdgcPFAWmOYSK7O1?utm_source=generator"
-          width="300" // Adjusted width for smaller screens
           height="100"
           allowfullscreen=""
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
-          className="frame ml-[5%] mb-[10%] md:ml-[22%]" // Adjusted margin for tablet screens
+          className="frame ml-[-10%]  mb-[10%] md:ml-[-20%] lg:ml-[5%]" // Adjusted margin for tablet screens
         ></iframe>
       </div>
     </>
   );
 };
-export default SpotifyEmbed;
+export default SpotifyEmbed2;
