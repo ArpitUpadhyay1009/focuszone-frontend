64862851be67e5613072829d38d97b40473c1c30
ArpitUpadhyay1009 <arpitupadhyay186@gmail.com>
2025-04-28 23:57:48 +0530

    fixed spotify look

--- src/assets/components/spotify2/SpotifyEmbed2.jsx
+++ src/assets/components/spotify2/SpotifyEmbed2.jsx
@@ -7,12 +7,11 @@ const SpotifyEmbed2 = () => {
       <div>
         <iframe
           src="https://open.spotify.com/embed/playlist/0oPyDVNdgcPFAWmOYSK7O1?utm_source=generator"
-          width="310" // Adjusted width for smaller screens
           height="100"
           allowfullscreen=""
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
-          className="frame ml-[10%]  mb-[10%] md:ml-[-20%] lg:ml-[17%]" // Adjusted margin for tablet screens
+          className="frame ml-[-10%]  mb-[10%] md:ml-[-20%] lg:ml-[3%]" // Adjusted margin for tablet screens
         ></iframe>
       </div>
     </>
